aec788117aa8e62412b63a6c9a12df627f7fe7da
Anton Shchukin <a.a.shchukin@gmail.com>
2025-08-06 17:03:18 +0700

    init with original components

---
+++ src/scripts/scripts.js
@@ -0,0 +1,52 @@
+(function($) {
+    $(document).ready(function() {
+
+        /* Select placeholder */
+        function selectPlaceholder($element) {
+            if ($element.val() === 'placeholder') {
+                $element.parent('.input').addClass('input--placeholder-is-chosen');
+            } else {
+                $element.parent('.input').removeClass('input--placeholder-is-chosen');
+            }
+        }
+
+        $('select.input__widget').each(function () {
+            selectPlaceholder($(this));
+        }).on('change', function () {
+            selectPlaceholder($(this));
+        });
+
+        /* Expanding textarea */
+        function expandTextarea($element) {
+            $element.css('height', 'auto');
+            $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+        }
+
+        $('.input--expandable .input__widget').each(function () {
+            expandTextarea($(this));
+        }).on('input', function () {
+            expandTextarea($(this));
+        });
+
+        /* Error field */
+        $('.input__widget').on('focus', function () {
+            $(this).parents('.input').removeClass('input--error');
+            $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+        });
+
+
+        /* Password eye */
+        $('.input__show-password').on('click', function() {
+            const $icon = $(this).find('.icon');
+            const $input = $(this).parents('.input').find('.input__widget');
+
+            if ($input.attr('type') === 'password') {
+                $icon.text('visibility');
+                $input.attr('type', 'text');
+            } else {
+                $icon.text('visibility_off');
+                $input.attr('type', 'password');
+            }
+        });
+    });
+})(jQuery);
